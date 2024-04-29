f3ab037bb1aa4c831160a57027a2d409b0976a56
tejas100 <tejas55bk@gmail.com>
2024-04-28 22:25:02 -0400

    change2

--- templates/conn.php
+++ templates/conn.php
@@ -1,8 +1,8 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'LibraryManagement');
+	// $conn = new mysqli('localhost', 'root', '', 'LibraryManagement');
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
+	// if ($conn->connect_error) {
+	//     die("Connection failed: " . $conn->connect_error);
+	// }
 	
 ?>
